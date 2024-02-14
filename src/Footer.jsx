65f7a462d9272b167ad604b7686413ef836f9ef1
Vinayak Aski <vinayakaski01@gmail.com>
2024-02-14 11:45:13 +0530

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>This site is created by Vinayak &copy; Copyright 2024</p>
+    </footer>
+  );
+}
